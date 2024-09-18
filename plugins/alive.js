427f58f808db39d34db85a78f5098380b4363cc5
Lakshan Damayantha 💗➤ <shadowwatsappbot@gmail.com>
2024-09-18 14:01:58 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,17 +11,15 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let des = `*👨‍💻 Hello WHITEALFHA  MD TESTING ❮❮💗*
-
-⚡𝗛𝗘𝗟𝗟𝗢𝗪 ┇ 𝗪𝗘𝗟𝗖𝗢𝗠𝗘 ⚡
-
-*💗 Nice to meat you*
-
-*► HI I'M ALOVE NOW👾*
-
-*🔖We welcome the new arrivals of the digital world_►*
-
-*❯ Welcome whitealfha md whats app bot you can download video songs and various videos through this bot. Shado md owner is Lakshan Damayantha.. It also gives you the ability to solve the new revolution in technology and technology problems and there is great potential here. This technology is also related to AI.*
+let des = `╭──────────────◎◎▷
+*👋 Hello 《................》🔰*
+╰──────────────◎◎▷
+*╔                                        ╕*
+   🔰I ᗩᗰ ᗩᒪIᐯᗴ ᑎOᗯ🔰
+*╘                                        ╜*
+*╭───────────┈⊷>*
+⚕️𝐖𝐄 𝐖𝐄𝐋𝐂𝐎𝐌 𝐓𝐇𝐄 𝐍𝐄𝐖 𝐀𝐑𝐑𝐈𝐕𝐀𝐋𝐒 𝐎𝐅 𝐓𝐇𝐄 𝐃𝐈𝐆𝐈𝐓𝐀𝐋 𝐖𝐎𝐑𝐋𝐃⚕️
+* ❯ *Welcome whitealfha md whats app bot you can download video songs and various videos through this bot.*
 
 > *➣ ᴏᴡɴᴇʀ : whitealfhaoficial*
 
@@ -31,9 +29,10 @@ let des = `*👨‍💻 Hello WHITEALFHA  MD TESTING ❮❮💗*
 
 > *➣ ᴠᴇʀꜱɪᴏɴ : 1.0.0*
 
-*┇Always smile and life will be beautiful💗*
+* ¤Always smile and life will be beautiful💗¤
 
-> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ whitealfhaoficial*✨`
+> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ whitealfhaoficial*  
+ *╰───────────┈⊷>*`
 
 return await conn.sendMessage(from,{image: {url: `https://i.ibb.co/p22J5nQ/file-1nh1gowb-Fqjz1-YJGzgo8u1e-J.jpg`},caption: des},{quoted: mek})
 }catch(e){
